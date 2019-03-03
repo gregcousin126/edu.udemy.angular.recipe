822fa992d317634111b7eef558fb1df082a85d9f
Gregoire <gregcousin126@gmail.com>
2019-03-03 11:21:43 +0000

    latest edit: gitu.all -> update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
+import {AppComponent} from './app.component';
+import {CockpitComponent} from './cockpit/cockpit.component';
+import {ServerElementComponent} from './server-element/server-element.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CockpitComponent,
-    ServerElementComponent
-  ],
-  
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  declarations: [AppComponent, CockpitComponent, ServerElementComponent],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+}
